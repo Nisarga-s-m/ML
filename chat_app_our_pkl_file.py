dbb2d5f51646a4a104b94d40a32983ed70cb0a51
Nisarga-s-m <nisargasm2@gmail.com>
2025-04-07 23:02:18 +0530

    Add files via upload

--- chat_app_our_pkl_file.py
+++ chat_app_our_pkl_file.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pickle
 import os
-from sklearn.feature_extraction.text import TfidfVectorizer
+
 
 # Load saved model & vectorizer
 @st.cache_resource  # Cache the model to avoid reloading on each interaction
